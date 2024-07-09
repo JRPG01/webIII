377450ce22a887508a51abe6e2c61adeec41c53d
Rafael Carrillo <s20120207@alumnos.itsur.edu.mx>
2024-07-08 19:58:34 -0600

    No se subieron las imagenes y recursos xD

---
+++ proyecto/view/aboutUs.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>¡Conocenos!</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+    integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
+    crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+  <link rel="stylesheet" href="../css/estilosAcerca.css">
+
+</head>
+<body>
+    <header>
+        <nav class="navbar fixed-top" style="background-color: #736151; padding-left: 30px;">
+            <div class="container-fluid">
+                <a class="navbar-brand text-white" href="../view/aboutUs.php">Hospital General</a>
+                <button class="navbar-toggler d-flex justify-content-center align-item-center text-align-center" onClick="window.location.href='login.php';">
+                    <h5 class = "m-1 text-black"> Iniciar Sesión </h5>
+                </button>
+            </div>
+        </nav>
+    </header>
+    <div class="d-flex justify-content-center align-item-center" style="flex-grow: 1;">
+
+        <div class="tarjetas">
+            <div class="bg-white d-flex flex-column justify-content-center align-items-center" style="width: 90%; border-radius: 10px; box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.5);">
+                <h1 style="padding-top: 10px;"> Somos Hospital General</h1>
+                <h1 style="0; font-weight: bold;"> ¡Conocenos! </h1>
+            </div>
+
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-front">
+                        <h2>Misión</h2>
+                    </div>
+                    <div class="card-back">
+                        <h2>Misión</h2>
+                        <p>
+                        •	Brindar atención médica de calidad y calidez humana a todos los pacientes, con un enfoque integral que promueva la salud, el bienestar y la recuperación.</br>
+                        •	Ser un referente en la prestación de servicios de salud, reconocido por su excelencia profesional, innovación tecnológica y compromiso con la ética y la responsabilidad social.</br>
+                        •	Contribuir a la formación de profesionales de la salud altamente calificados y comprometidos con la atención humanizada y de calidad.</br>
+                        •	Fomentar la investigación científica y la transferencia de conocimiento para el desarrollo de nuevas estrategias de prevención, diagnóstico y tratamiento de enfermedades.
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-front">
+                        <h2>Visión</h2>
+                    </div>
+                    <div class="card-back">
+                        <h2>Visión</h2>
+                        <p>
+                        •	Ser el hospital líder en la región, reconocido por su excelencia en la atención médica, su compromiso con la calidad y su contribución al bienestar de la comunidad.</br>
+                        •	Contar con un equipo humano altamente calificado y comprometido con la atención integral y humanizada de los pacientes.</br>
+                        •	Disponer de tecnología de vanguardia para el diagnóstico y tratamiento de enfermedades.</br>
+                        •	Ser un centro de referencia para la formación de profesionales de la salud y la investigación científica.</br>
+                        •	Contribuir a la construcción de una sociedad más saludable a través de la promoción de la salud y la prevención de enfermedades.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-front">
+                        <h2>Objetivos</h2>
+                    </div>
+                    <div class="card-back">
+                        <h2>Objetivos</h2>
+                        <p>
+                        •	Reducir en un 10% la tasa de mortalidad hospitalaria en los próximos 5 años.</br>
+                        •	Disminuir en un 20% el tiempo de espera promedio para citas médicas en el próximo año.</br>
+                        •	Aumentar en un 15% el número de pacientes que reciben atención médica especializada en el próximo año.</br>
+                        •	Obtener la certificación internacional de calidad en los próximos 3 años.</br>
+                        •	Publicar 10 artículos científicos en revistas indexadas en el próximo año.</br>
+                        •	Capacitar al 100% del personal médico y de enfermería en los últimos avances médicos en el próximo año.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-front">
+                        <h2>Servicios médicos</h2>
+                    </div>
+                    <div class="card-back">
+                        <h2>Servicios médicos</h2>
+                        <p>
+                        •	Medicina general: Brindamos atención médica integral para pacientes de todas las edades, incluyendo chequeos preventivos, diagnóstico y tratamiento de enfermedades comunes.</br>
+                        •	Especialidades médicas: Contamos con un equipo de médicos especialistas altamente calificados en diversas áreas.</br>
+                        •	Servicios quirúrgicos: Realizamos una amplia gama de procedimientos quirúrgicos, tanto ambulatorios como de alta complejidad, utilizando tecnología de vanguardia y con un enfoque en la seguridad del paciente.</br>
+                        •	Urgencias: Disponemos de un servicio de urgencias las 24 horas del día, los 7 días de la semana, para atender emergencias médicas de manera oportuna y eficaz.
+
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-inner">
+                    <div class="card-front">
+                        <h2>Servicios para citas</h2>
+                    </div>
+                    <div class="card-back">
+                        <h2>Servicios para citas</h2>
+                        <p>
+                        •	Agenda tu cita en línea: Puedes agendar tu cita con el médico de tu preferencia a través de nuestro sitio web o aplicación móvil.</br>
+                        •	Cita por teléfono: También puedes agendar tu cita llamando a nuestro centro de atención telefónica.</br>
+                        •	Cita presencial: Si lo prefieres, puedes agendar tu cita directamente en el hospital.</br>
+                        •	Cancelación de citas: Puedes cancelar o reprogramar tu cita en línea, por teléfono o en persona.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<footer class="text-center" style=" z-index: 0;">
+  <!-- Grid container -->
+  <div class="container p-2 pb-0">
+    <!-- Section: Social media -->
+    <section class="mb-2">
+      <!-- Facebook -->
+      <a data-mdb-ripple-init class="btn text-white btn-floating m-1" style="background-color: #3b5998;" href="https://www.facebook.com/share/r/Ugdm3bonXXC5Kndb/?mibextid=oFDknk"
+        role="button"><i class="fab fa-facebook-f"></i></a>
+
+      <!-- Twitter -->
+      <a class="btn text-white btn-floating m-1" style="background-color: #55acee;" href="https://x.com/weirddalle/status/1809603211403374841?t=EqC64-GOJqjfhPkfXC8YzA&s=08" role="button"><i
+          class="fab fa-twitter"></i></a>
+
+      <!-- Google -->
+      <a class="btn text-white btn-floating m-1" style="background-color: #dd4b39;" href="https://x.com/ghost_v52/status/1810030017545752922?t=NabtkSo0q9QeaSuR0Q_Qig&s=08" role="button"><i
+          class="fab fa-google"></i></a>
+
+      <!-- Instagram -->
+      <a class="btn text-white btn-floating m-1" style="background-color: #ac2bac;" href="https://www.instagram.com/reel/C8sJKYcJ3mJ/?igsh=NTc4MTIwNjQ2YQ==" role="button"><i
+          class="fab fa-instagram"></i></a>
+
+      <!-- Linkedin -->
+      <a class="btn text-white btn-floating m-1" style="background-color: #0082ca;" href="https://x.com/ghost_v52/status/1810030017545752922?t=NabtkSo0q9QeaSuR0Q_Qig&s=08" role="button"><i
+          class="fab fa-linkedin-in"></i></a>
+    </section>
+    <!-- Section: Social media -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3 text-white" style="background-color: #403D38;">
+    © 2024 Copyright:
+    <a class="text-white" href="">ITSUR WEB III</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+
+</html>
